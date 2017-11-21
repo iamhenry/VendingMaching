954e634b64cb56a6e45256404c4fe699e01062aa
henry moran <virtualhenry@gmail.com>
2017-11-21 09:29:58 -0800

    - Created the vending machine protocols along with the enums that contain the items in the vending machine

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,39 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Henry Moran on 11/21/17.
+//  Copyright © 2017 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var invetory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+}
